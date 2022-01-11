4ec223241435cb11205e3dbc78fdea49559e55be
JeongPope <nullptr-@kakao.com>
2022-01-11 11:06:23 +0900

    add: logger used logrus

---
+++ logger/caller.go
@@ -0,0 +1,15 @@
+package devtool
+
+import (
+	"runtime"
+	"strconv"
+)
+
+func GetCaller() string {
+	pc, _, _, _ := runtime.Caller(2)
+	runtimeFunc := runtime.FuncForPC(pc)
+	_, runtimeLine := runtimeFunc.FileLine(pc)
+	runtimeName := runtimeFunc.Name()
+
+	return runtimeName + ":" + strconv.Itoa(runtimeLine)
+}
